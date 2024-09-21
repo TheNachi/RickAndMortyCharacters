c90715065b1fc01c12b722dd9bacf3d99dced73a
Munachimso Ugorji <ugorjimunachimso@gmail.com>
2024-09-21 12:59:41 +0100

    Initial Commit

---
+++ RickAndMortyCharacters/RickAndMortyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharactersApp.swift
+//  RickAndMortyCharacters
+//
+//  Created by Munachimso Ugorji on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
